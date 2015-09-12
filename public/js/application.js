acc4df78997fad9e93362207c28047f849685acc
julia-castro <julia.castro@outlook.com>
2015-09-12 19:26:28 -0400

    add second api hit

--- public/js/application.js
+++ public/js/application.js
@@ -6,10 +6,22 @@ $(document).ready(function() {
       type: 'GET',
       dataType: 'jsonp',
       success: function (data, textStatus, xhr) {
-        $("h3").text("Local Health and Wellness Meetups");
+        $("#meetups").text("Local Health and Wellness Meetups");
         for (var i = 0; i < data["results"].length; i++){
-          // console.log(data["results"][i]["name"]);
-          $("ul").append('<li>' + '<a href="' + data["results"][i]["link"] + '">' + data["results"][i]["name"] + '</a>' + '</li>');
+          $("#meetups_list").append('<li>' + '<a href="' + data["results"][i]["link"] + '">' + data["results"][i]["name"] + '</a>' + '</li>');
+        };
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log('Error in Operation');
+      }
+    });
+    $.ajax({
+      url: 'https://api.meetup.com/2/groups?key=4e2b46647072193c161c4a13d397662&sign=true&photo-host=public&zip=' + user_input + '&radius=1&category_id=14',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function (data, textStatus, xhr) {
+        for (var i = 0; i < data["results"].length; i++){
+          $("#meetups_list").append('<li>' + '<a href="' + data["results"][i]["link"] + '">' + data["results"][i]["name"] + '</a>' + '</li>');
         };
       },
       error: function (xhr, textStatus, errorThrown) {
